c35c328b3c9cf9ca986897bc4a936e0ed3e783f0
ramakrishna <ramakri4u@yahoo.com>
2020-08-22 07:19:13 +0000

    commands to run on all nodes

---
+++ k8s-runonnodes.sh
@@ -0,0 +1,10 @@
+#Before executing the below token command
+#you should execute the k8s-install.sh file on all nodes.
+#This is the sample token command to run on all nodes if you want
+#those node to join in the kubernetes cluster.
+
+#k8s-node1 and k8s-node2 ....
+kubeadm join 10.128.0.57:6443 --token l37j0z.mxn8nz85ky53ghi7 \
+    --discovery-token-ca-cert-hash sha256:3788586afe28993ae0cc47f902c82d5f291641580e2f8bad1384eced9a70e731
+
+
