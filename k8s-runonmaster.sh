7e66bb3155a6fded26914db13515fed7ee48a819
ramakrishna <ramakri4u@yahoo.com>
2020-08-22 07:11:59 +0000

    commands to run on master node

---
+++ k8s-runonmaster.sh
@@ -0,0 +1,18 @@
+kubeadm init
+
+#This will generate 3 commands.
+#Run those commands on Master node only.
+#And copy the token string generated and save it for further usage
+#to add any node to the kubernetes cluster.
+
+# mkdir -p $HOME/.kube
+# sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+# sudo chown $(id -u):$(id -g) $HOME/.kube/config
+# After executing the above commands, execute the below commands
+
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
+
+
